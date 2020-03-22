cc99cedb41cbabf4b529c6d10326b6804a7bbfa2
elizzaveta <lizatimcenko5@gmail.com>
2020-03-22 19:19:30 +0200

    Added  arguments() to get expression from cmd

--- lab3/func.cpp
+++ lab3/func.cpp
@@ -2,3 +2,13 @@
 #include "func.h"
 
 using namespace std;
+
+//to get the expression from cmd
+string arguments(int argc, char* argv[]) {
+	string s = "";
+
+	for (int i = 1; i < argc; ++i) {
+		s += argv[i];
+	}
+	return s;
+}
--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -4,21 +4,9 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
     std::cout << "Hello World!\n";
 
-
-
-    //some test code to check if stack works after pull request from github
-    Stack stek(3);
-    for (int i = 0; i < 3; i++) {
-        stek.push(i + 1);
-    }
-    int a = stek.size();
-    for (int i = 0; i < a; i++) {
-        cout << "elem to go: " << stek.pop() << endl;
-    }
-    stek.clear();
-    //delete after check
+    
 }
