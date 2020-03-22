6d3eac37270c2f7b3f738dc2ed7005e18c37c0f8
elizzaveta <lizatimcenko5@gmail.com>
2020-03-22 10:16:50 +0200

    Added test code

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -8,4 +8,17 @@ int main()
 {
     std::cout << "Hello World!\n";
 
+
+
+    //some test code to check if stack works after pull request from github
+    Stack stek(3);
+    for (int i = 0; i < 3; i++) {
+        stek.push(i + 1);
+    }
+    int a = stek.size();
+    for (int i = 0; i < a; i++) {
+        cout << "elem to go: " << stek.pop() << endl;
+    }
+    stek.clear();
+    //delete after check
 }
